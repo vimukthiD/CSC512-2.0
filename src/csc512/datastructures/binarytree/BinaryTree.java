d7a973bf488c1c5be026ffca109dee192cf47222
vimukthiD <dissanayake.kasun@gmail.com>
2016-07-22 12:11:56 +0530

    1. add mirror method to binary tree 2. fix delete method's missing child issue

--- src/csc512/datastructures/binarytree/BinaryTree.java
+++ src/csc512/datastructures/binarytree/BinaryTree.java
@@ -64,6 +64,8 @@ public class BinaryTree {
             return;
         } else if (currentNode.leftNode != null && currentNode.rightNode != null) {
             Node successor = removeSuccessor(currentNode);
+            successor.leftNode = currentNode.leftNode;
+            successor.rightNode = currentNode.rightNode;
             setNewChildNode(parentNode, isLeftChild, successor);
         } else if (currentNode.leftNode != null) {
             setNewChildNode(parentNode, isLeftChild, currentNode.leftNode);
@@ -109,6 +111,10 @@ public class BinaryTree {
             printPostOrder(node.rightNode);
             System.out.print(" " + node.data);
         }
+    }
+
+    public void mirror() {
+        rootNode.mirror(rootNode);
     }
 
     private Node removeSuccessor(Node currentNode) {
